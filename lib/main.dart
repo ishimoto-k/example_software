63e9395de57639b85e72469259ec65cf1b6369a9
KikoIshimoto <ishimoto.life@gmail.com>
2024-02-13 19:17:35 +0900

    fix refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,166 +1,191 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePageView(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class NumberModel{
+// モデルクラス
+class NumberModel {
   int number;
   NumberModel(this.number);
 }
-class CounterModel{
+
+class CounterModel {
   int number;
   CounterModel(this.number);
 }
 
-class NumberRepository{
+// リポジトリクラス
+class NumberRepository {
   NumberModel _model = NumberModel(0);
-  void setNumber(int c){
-    _model.number = c;
+
+  void setNumber(int number) {
+    _model.number = number;
   }
-  int getNumber(){
+
+  int getNumber() {
     return _model.number;
   }
 }
-class CounterRepository{
+
+class CounterRepository {
   CounterModel _model = CounterModel(0);
-  void setNumber(int c){
-    _model.number = c;
+
+  void setNumber(int number) {
+    _model.number = number;
   }
-  int getNumber(){
+
+  int getNumber() {
     return _model.number;
   }
 }
 
-class CountUpUsecase{
-  NumberRepository _repository = NumberRepository();
-  void increment(){
-    _repository.setNumber(_repository.getNumber()+1);
+// ユースケースクラス
+class CounterUseCase {
+  NumberRepository _numberRepository = NumberRepository();
+
+  void increment() {
+    _numberRepository.setNumber(_numberRepository.getNumber() + 1);
   }
-  int get nowNumber => _repository.getNumber();
+
+  int getCurrentNumber() {
+    return _numberRepository.getNumber();
+  }
 }
 
-class TimerService{
+// サービスクラス
+class TimerService {
   void Function(int) callback;
   late Timer _timer;
-  CounterRepository _repository = CounterRepository();
+  CounterRepository _counterRepository = CounterRepository();
+
   TimerService(this.callback);
 
   void _tick(Timer timer) {
-    _repository.setNumber(_repository.getNumber()+1);
-    callback(_repository.getNumber());
+    _counterRepository.setNumber(_counterRepository.getNumber() + 1);
+    callback(_counterRepository.getNumber());
   }
-  void startTimer(){
+
+  void startTimer() {
     _timer = Timer.periodic(Duration(seconds: 1), _tick);
   }
-  void timerCancel(){
+
+  void timerCancel() {
     _timer.cancel();
   }
-  int get nowNumber => _repository.getNumber();
+
+  int getCurrentNumber() {
+    return _counterRepository.getNumber();
+  }
 }
 
-class ViewModel{
-  int timerNumber = 0;
-  int counterNumber = 0;
+// ViewModelクラス
+class ViewModel {
+  int timerNumber;
+  int counterNumber;
   ViewModel(this.timerNumber, this.counterNumber);
 }
 
-class Controller{
+// Controllerクラス
+class Controller {
   late void Function(ViewModel) _callback;
-  CountUpUsecase usecase = CountUpUsecase();
-  late TimerService service;
+  CounterUseCase _counterUseCase = CounterUseCase();
+  late TimerService _timerService;
 
-  void _tick(int counter){
-    _callback(ViewModel(service.nowNumber, usecase.nowNumber));
+  void _tick(int counter) {
+    _callback(ViewModel(_timerService.getCurrentNumber(), _counterUseCase.getCurrentNumber()));
   }
-  Controller(void Function(ViewModel) callback){
+
+  Controller(void Function(ViewModel) callback) {
     _callback = callback;
-    service = TimerService(_tick);
+    _timerService = TimerService(_tick);
   }
-  void incrementCounter(){
-    usecase.increment();
-    _callback(ViewModel(service.nowNumber, usecase.nowNumber));
+
+  void incrementCounter() {
+    _counterUseCase.increment();
+    _callback(ViewModel(_timerService.getCurrentNumber(), _counterUseCase.getCurrentNumber()));
   }
-  void startTimer(){
-    service.startTimer();
+
+  void startTimer() {
+    _timerService.startTimer();
+  }
+
+  void dispose() {
+    _timerService.timerCancel();
   }
 }
 
+// メインのアプリケーション
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePageView(title: 'Counter Example'),
+    );
+  }
+}
 
 class MyHomePageView extends StatefulWidget {
-  const MyHomePageView({super.key, required this.title});
   final String title;
+
+  const MyHomePageView({Key? key, required this.title}) : super(key: key);
+
   @override
-  State<MyHomePageView> createState() => _MyHomePageState();
+  State<MyHomePageView> createState() => _MyHomePageViewState();
 }
 
-class _MyHomePageState extends State<MyHomePageView> {
-  ViewModel state = ViewModel(0,0);
-  late Controller controller;
-  _MyHomePageState(){
-    controller = Controller((ViewModel viewModel) {
+class _MyHomePageViewState extends State<MyHomePageView> {
+  ViewModel _viewModel = ViewModel(0, 0);
+  late Controller _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = Controller((ViewModel viewModel) {
       setState(() {
-        state = viewModel;
+        _viewModel = viewModel;
       });
     });
-    controller.startTimer();
+    _controller.startTimer();
   }
 
-  void event() {
-    controller.incrementCounter();
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _incrementCounter() {
+    _controller.incrementCounter();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '${state.counterNumber}',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Counter: ${_viewModel.counterNumber}',
+              style: TextStyle(fontSize: 24),
             ),
-            const Text(
-              'ticker counter',
-            ),
+            SizedBox(height: 20),
             Text(
-              '${state.timerNumber}',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Timer: ${_viewModel.timerNumber}',
+              style: TextStyle(fontSize: 24),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: event,
+        onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ),
     );
   }
